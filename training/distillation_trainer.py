3089173c0f15cefcff6a605b15a8369d583e47f7
AbhirKarande <abhir511@gmail.com>
2025-02-03 16:29:07 -0800

    qlora

--- training/distillation_trainer.py
+++ training/distillation_trainer.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import Trainer
+from transformers import Trainer, BitsAndBytesConfig
 
 class DistillationTrainer(Trainer):
     def __init__(self, teacher_model, *args, **kwargs):
@@ -23,3 +23,21 @@ class DistillationTrainer(Trainer):
         total_loss = answer_loss + 0.5 * hidden_loss
         
         return (total_loss, outputs) if return_outputs else total_loss
+
+class QuantizedDistillationTrainer(DistillationTrainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def _load_model(self, model, args):
+        # Load base model with 4-bit quantization
+        from transformers import AutoModelForCausalLM
+        model = AutoModelForCausalLM.from_pretrained(
+            model.config._name_or_path,
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_compute_dtype=torch.bfloat16,
+                bnb_4bit_quant_type="nf4"
+            ),
+            device_map="auto"
+        )
+        return model
--- training/lora_adapters.py
+++ training/lora_adapters.py
@@ -1,13 +1,26 @@
 from peft import LoraConfig, get_peft_model
+from transformers import BitsAndBytesConfig
+import torch
 
-def create_lora_adapter(base_model, adapter_type: str):
+def create_qlora_adapter(base_model, adapter_type: str):
+    # Quantization config
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=True
+    )
+    
+    # QLoRA config
     config = LoraConfig(
-        r=64,
+        r=64,  # Reduced from original 64 to 32 for quantization
         lora_alpha=32,
         target_modules=["q_proj", "v_proj"],
         lora_dropout=0.05,
         bias="none",
         task_type="CAUSAL_LM",
-        adapter_name=adapter_type
+        adapter_name=adapter_type,
+        quantization_config=bnb_config  # Add quantization config
     )
+    
     return get_peft_model(base_model, config)
